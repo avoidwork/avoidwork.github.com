1b861c7384aa05020f3e1ad95b9865c52d1a174a
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-04-27 15:43:26 -0400

    Purging

--- assets/dashboard.js
+++
@@ -1,3 +0,0 @@
-$.on("ready", function(){ $("body")[0].hide(); });
-$.on("render",  function(){ $("body")[0].opacity(0).show().fade(1000); });
-
--- assets/test.js
+++
@@ -1,7 +0,0 @@
-var obj = document.createElement("div");
-obj.innerHTML = "<iframe width='300' id='testing' height='50' src='about:blank'></iframe>";
-obj.childNodes[0].id = "testing";
-document.body.appendChild(obj);
-setTimeout(function(){
-	document.getElementById("testing").contentWindow.document.body.innerHTML = "testing";
-}, 250);
